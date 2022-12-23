43bd634b4a4b5ebc32a6318e17fd8b03167fe62c
KodyAnderson1 <kody_anderson@yahoo.com>
2022-12-23 09:50:41 -0600

    Added project - Notes

--- src/model/projectsData.js
+++ src/model/projectsData.js
@@ -3,26 +3,43 @@ import trainReservations from "../images/project_imgs/train-reservations.png";
 import taskifyPicture from "../images/project_imgs/taskify.png";
 import gradeCalculator from "../images/project_imgs/grade-calculator.png";
 import student from "../images/project_imgs/student_full_stack.png";
+import notesApp from "../images/project_imgs/Notes_markdown_app.png";
 
 const portfolioWebsiteDescription =
-  "This website was built with React and Bootstrap and primarily made following a tutorial on Udemy. I Mainly learned CSS styling (using custom classes) and making minor changes using Bootstrap shorthand. The Experience & Timeline section is a prominent example of new concepts: z-index, text manipulation, and predefined colors.";
+  "This website was built with React and Bootstrap and primarily made following a tutorial on Udemy. " +
+  "I Mainly learned CSS styling (using custom classes) and making minor changes using Bootstrap shorthand." +
+  " The Experience & Timeline section is a prominent example of new concepts: z-index, text manipulation, and predefined colors.";
 const TrainReservationsDescription =
-  "This project was built in concert with four others during a semester-long group project in Software Engineering. We utilized agile methodology, specifically SCRUM, for our project management methodology. We also used Trello as a project management tool, Github (heavily) as version control, and Java as our programming language.";
+  "This project was built in concert with four others during a semester-long group project in Software Engineering." +
+  " We utilized agile methodology, specifically SCRUM, for our project management methodology." +
+  " We also used Trello as a project management tool, Github (heavily) as version control, and Java as our programming language.";
 const taskifyDescription =
-  "This website was built with React and Typescript and was primarily made to familiarize myself with typescript following a youtube tutorial. I also learned how to create drag-and-drop components during the process—hosted by firebase.";
+  "This website was built with React and Typescript and was primarily made to familiarize myself with typescript following a youtube tutorial." +
+  " I also learned how to create drag-and-drop components during the process—hosted by firebase.";
+const StudentDescription =
+  "This project is a simple full-stack application built with Java Spring Boot, React, and MySQL." +
+  " This project is a WIP, and I plan to expand on it to create a student management application for students, faculty, and admins." +
+  " To see installation instructions, view the readme for the project on GitHub.";
+
 const GCDescription =
-  "This website was built as a class project for Client-Side Programming. We learned HTML, CSS, Javascript, and Bootstrap with a significant focus on React. We also learned about Firebase Hosting, it's real-time database, and user authentication. All of which is integrated into the app.";
+  "This website was built as a class project for Client-Side Programming." +
+  " We learned HTML, CSS, Javascript, and Bootstrap with a significant focus on React." +
+  " We also learned about Firebase Hosting, it's real-time database, and user authentication." +
+  " All of which is integrated into the app.";
 const GCAdditional = "To view with prefilled data:";
 const GcUlList = [
   'Click "Sign in with email"',
   "Email: gradecalculator@example.com",
   "Password: 123456",
 ];
-const StudentDescription =
-  "This project is a simple full-stack application built with Java Spring(boot), React (with Materials Icon for prebuilt UI components), and MySQL. This project is a WIP, and I plan to expand on it to create a student management application for students, faculty, and admins. To see installation instructions, view the readme for the project on GitHub.";
 
-const noWestCollectiveDescription =
-  "This is a website for a club at The University of West Florida (UWF). The site was built with React and Tailwind CSS.";
+const NotesAppDesc =
+  "This note-taking application uses a react library to implement markdown in the body to style the text as the user sees fit." +
+  " It can search by the title of the notes or by the tags the user creates when making or editing the note." +
+  " Tags are also able to be edited or deleted. End goal of this project will be to connect itself to a database instead of using local storage.";
+
+// const noWestCollectiveDescription =
+//   "This is a website for a club at The University of West Florida (UWF). The site was built with React and Tailwind CSS.";
 
 export const projects = [
   {
@@ -68,6 +85,16 @@ export const projects = [
     tutorialName: "Youtube",
     tutorialURL: "https://www.youtube.com/watch?v=O_XL9oQ1_To",
   },
+  {
+    projectName: "Notes App",
+    websiteURL: "https://notesapp-c9688.web.app/",
+    githubURL: "https://github.com/KodyAnderson1/MarkdownNotesApp",
+    description: NotesAppDesc,
+    imagePath: notesApp,
+    tutorialName: "Youtube",
+    tutorialURL: "https://www.youtube.com/watch?v=j898RGRw0b4",
+  },
+
   // {
   //   projectName: "No.West Collective",
   //   githubURL: "https://github.com/KodyAnderson1/",
